4fa9a42d60f415fcc002314f96d120fbf5282f5c
Josue Rodriguez <jr2589213@gmail.com>
2020-08-19 13:53:36 -0700

    Got print.ls8 running
    * Implement the CPU constructor
    * Add RAM functions ram_read() and ram_write()
    * Implement the core of run()
    * Implement the HLT instruction handler
    * Add the LDI instruction
    * Add the PRN instruction

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,19 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +25,25 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+        print(op)
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +55,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +69,39 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while True:
+            # ir set to ram address stored in register
+            ir = self.ram[self.pc]
+
+            if ir == LDI:
+                # set operand_a and use ram read with addy at self.pc + 1
+                operand_a = self.ram_read(self.pc + 1)
+                # set operand_b and use ram read with addy at self.pc + 2
+                operand_b = self.ram_read(self.pc + 2)
+                # set value of a register
+                self.reg[operand_a] = operand_b
+
+                self.pc += 2
+            elif ir == PRN:
+                current_value = self.ram_read(self.pc + 1)
+
+                print(self.reg[current_value])
+
+                self.pc += 1
+            elif ir == HLT:
+                break
+            else:
+                break
+
+            self.pc += 1
+
+            self.trace()
+
+    def ram_read(self, addy):
+        return self.ram[addy]
+
+    def ram_write(self, addy, value):
+        if addy not in self.ram or self.ram[addy]:
+            self.ram[addy] = value
+        else:
+            print('Hey item something went wrong, check ram_write function')
